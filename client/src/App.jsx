c42562bf9cb2ec296883b54709197653b85c2e29
Hassaan-Bin-Kaleem <105790628+HassaanBinKaleem@users.noreply.github.com>
2024-11-13 22:38:01 +0500

    Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import BasicExample from "../components/BasicExample";
+
+const Layout = () => {
+  return (
+    <>
+      <div>
+        <BasicExample />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
