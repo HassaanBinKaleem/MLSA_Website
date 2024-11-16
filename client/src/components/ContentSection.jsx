80756b3d8ab5d987e28241da376ce7365d837003
Aarish Mughal <aarishmughal21@gmail.com>
2024-11-17 03:07:54 +0500

    Somewhat aboutus complete

--- client/src/components/ContentSection.jsx
+++ client/src/components/ContentSection.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import Events from "./sections/Events";
+import AboutUs from "./sections/AboutUs";
 
 const ContentSection = () => {
     return (
         <div className="content-section">
             <Hero />
             <Events />
+            <AboutUs />
         </div>
     );
 };
